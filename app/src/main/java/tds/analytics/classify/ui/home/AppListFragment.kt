4f35dd282f79817b4bcc636d33871c273214a9eb
Tonghe12138 <1178149120@qq.com>
2025-12-08 17:49:42 +0800

    applist添加100数字（listview）

--- app/src/main/java/tds/analytics/classify/ui/home/AppListFragment.kt
+++ app/src/main/java/tds/analytics/classify/ui/home/AppListFragment.kt
@@ -10,12 +10,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import tds.analytics.classify.databinding.FragmentApplistBinding
 import tds.analytics.classify.databinding.FragmentHomeBinding
+import android.widget.ArrayAdapter
 
 class FragmentAppList : Fragment() {
 
     private var _binding: FragmentApplistBinding? = null
 
     private val binding get() = _binding!!
+    private val numberList = (1..100).map { "数字 $it" }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +26,19 @@ class FragmentAppList : Fragment() {
     ): View? {
         _binding = FragmentApplistBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setuplistview()
+    }
+    private fun setuplistview(){
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            numberList
+        )
+        binding.listView.adapter = adapter
     }
 }
